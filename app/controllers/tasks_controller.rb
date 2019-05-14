e9a0757cb06a5973b459af5e8259b3f24ef9306b
Stephan Lotze <stephan.lotze@outlook.com>
2019-05-14 21:19:27 +0200

    exercise 2

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(title: params[:task][:title], details: params[:task][:details])
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(title: params[:task][:title], details: params[:task][:details], completed: params[:task][:completed])
+    @task.save
+    redirect_to update_task_path(@task)
+  end
+
+  def delete
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create', as: :create_task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update', as: :update_task
+  delete 'tasks/:id', to: 'tasks#delete', as: :delete_task
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
